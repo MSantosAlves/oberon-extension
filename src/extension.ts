06ab4abb4edecca5d22e0bb4236f08aba40f17f7
richardjlv <richardjunio39@gmail.com>
2023-06-28 20:14:20 -0300

    adding compile and run program

--- src/extension.ts
+++ src/extension.ts
@@ -4,29 +4,28 @@
 /*
  * extension.ts (and activateMockDebug.ts) forms the "plugin" that plugs into VS Code and contains the code that
  * connects VS Code with the debug adapter.
- * 
+ *
  * extension.ts contains code for launching the debug adapter in three different ways:
  * - as an external program communicating with VS Code via stdin/stdout,
  * - as a server process communicating with VS Code via sockets or named pipes, or
  * - as inlined code running in the extension itself (default).
- * 
+ *
  * Since the code in extension.ts uses node.js APIs it cannot run in the browser.
  */
 
-'use strict';
+"use strict";
 
-import * as Net from 'net';
-import * as vscode from 'vscode';
-import { randomBytes } from 'crypto';
-import { tmpdir } from 'os';
-import { join } from 'path';
-import { platform } from 'process';
-import { ProviderResult } from 'vscode';
-import { MockDebugSession } from './mockDebug';
-import { activateMockDebug, workspaceFileAccessor } from './activateMockDebug';
+import * as Net from "net";
+import * as vscode from "vscode";
+import { randomBytes } from "crypto";
+import { tmpdir } from "os";
+import { join } from "path";
+import { platform } from "process";
+import { ProviderResult } from "vscode";
+import { MockDebugSession } from "./mockDebug";
+import { activateMockDebug, workspaceFileAccessor } from "./activateMockDebug";
 
-
-const runMode: 'external' | 'server' | 'namedPipeServer' | 'inline' = 'inline';
+const runMode: "external" | "server" | "namedPipeServer" | "inline" = "inline";
 
 export function activate(context: vscode.ExtensionContext) {
   let complete = vscode.languages.registerCompletionItemProvider("oberon", {
@@ -92,41 +91,43 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(complete);
 
   switch (runMode) {
-    case 'server':
+    case "server":
       activateMockDebug(context, new MockDebugAdapterServerDescriptorFactory());
       break;
 
-    case 'namedPipeServer':
-      activateMockDebug(context, new MockDebugAdapterNamedPipeServerDescriptorFactory());
+    case "namedPipeServer":
+      activateMockDebug(
+        context,
+        new MockDebugAdapterNamedPipeServerDescriptorFactory()
+      );
       break;
 
-    case 'external': default:
+    case "external":
+    default:
       activateMockDebug(context, new DebugAdapterExecutableFactory());
       break;
 
-    case 'inline':
+    case "inline":
       activateMockDebug(context);
       break;
   }
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
 
-class DebugAdapterExecutableFactory implements vscode.DebugAdapterDescriptorFactory {
-
-
-  createDebugAdapterDescriptor(_session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): ProviderResult<vscode.DebugAdapterDescriptor> {
-
+class DebugAdapterExecutableFactory
+  implements vscode.DebugAdapterDescriptorFactory
+{
+  createDebugAdapterDescriptor(
+    _session: vscode.DebugSession,
+    executable: vscode.DebugAdapterExecutable | undefined
+  ): ProviderResult<vscode.DebugAdapterDescriptor> {
     if (!executable) {
       const command = "absolute path to my DA executable";
-      const args = [
-        "some args",
-        "another arg"
-      ];
+      const args = ["some args", "another arg"];
       const options = {
         cwd: "working directory for executable",
-        env: { "envVariable": "some value" }
+        env: { envVariable: "some value" },
       };
       executable = new vscode.DebugAdapterExecutable(command, args, options);
     }
@@ -135,21 +136,26 @@ class DebugAdapterExecutableFactory implements vscode.DebugAdapterDescriptorFact
   }
 }
 
-class MockDebugAdapterServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
-
+class MockDebugAdapterServerDescriptorFactory
+  implements vscode.DebugAdapterDescriptorFactory
+{
   private server?: Net.Server;
 
-  createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-
+  createDebugAdapterDescriptor(
+    session: vscode.DebugSession,
+    executable: vscode.DebugAdapterExecutable | undefined
+  ): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
     if (!this.server) {
-      this.server = Net.createServer(socket => {
+      this.server = Net.createServer((socket) => {
         const session = new MockDebugSession(workspaceFileAccessor);
         session.setRunAsServer(true);
         session.start(socket as NodeJS.ReadableStream, socket);
       }).listen(0);
     }
 
-    return new vscode.DebugAdapterServer((this.server.address() as Net.AddressInfo).port);
+    return new vscode.DebugAdapterServer(
+      (this.server.address() as Net.AddressInfo).port
+    );
   }
 
   dispose() {
@@ -159,24 +165,32 @@ class MockDebugAdapterServerDescriptorFactory implements vscode.DebugAdapterDesc
   }
 }
 
-class MockDebugAdapterNamedPipeServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
-
+class MockDebugAdapterNamedPipeServerDescriptorFactory
+  implements vscode.DebugAdapterDescriptorFactory
+{
   private server?: Net.Server;
 
-  createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-
+  createDebugAdapterDescriptor(
+    session: vscode.DebugSession,
+    executable: vscode.DebugAdapterExecutable | undefined
+  ): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
     if (!this.server) {
-      const pipeName = randomBytes(10).toString('utf8');
-      const pipePath = platform === "win32" ? join('\\\\.\\pipe\\', pipeName) : join(tmpdir(), pipeName);
+      const pipeName = randomBytes(10).toString("utf8");
+      const pipePath =
+        platform === "win32"
+          ? join("\\\\.\\pipe\\", pipeName)
+          : join(tmpdir(), pipeName);
 
-      this.server = Net.createServer(socket => {
+      this.server = Net.createServer((socket) => {
         const session = new MockDebugSession(workspaceFileAccessor);
         session.setRunAsServer(true);
         session.start(<NodeJS.ReadableStream>socket, socket);
       }).listen(pipePath);
     }
 
-    return new vscode.DebugAdapterNamedPipeServer(this.server.address() as string);
+    return new vscode.DebugAdapterNamedPipeServer(
+      this.server.address() as string
+    );
   }
 
   dispose() {
